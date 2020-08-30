3dfcf306926ca8962860bdb459f861bdb699ffc7
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-08-29 21:23:37 -0500

    Updates

---
+++ precache-manifest.0b0f5988244d997acbca05bcd24ea6c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786f06d22385bc15fb2ddae6cd82ba0f",
+    "url": "/Employee_Directory/index.html"
+  },
+  {
+    "revision": "8f97a8dbd4711bee888b",
+    "url": "/Employee_Directory/static/css/main.bdd63d93.chunk.css"
+  },
+  {
+    "revision": "7381cc897e7307add27b",
+    "url": "/Employee_Directory/static/js/2.7fd6be29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee_Directory/static/js/2.7fd6be29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f97a8dbd4711bee888b",
+    "url": "/Employee_Directory/static/js/main.9fd27a73.chunk.js"
+  },
+  {
+    "revision": "729a714dbeccef1f7001",
+    "url": "/Employee_Directory/static/js/runtime-main.eabc2322.js"
+  }
+]);
